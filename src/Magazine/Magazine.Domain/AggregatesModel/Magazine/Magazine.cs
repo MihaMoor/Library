d0bed007fa72233ca9f674dea87036f7d92f0e2a
Моор Михаил Александрович <m.moor@geomix.ru>
2024-04-17 12:58:27 +0700

    feature: добавлены модели для сервиса Magazine

---
+++ src/Magazine/Magazine.Domain/AggregatesModel/Magazine/Magazine.cs
@@ -0,0 +1,8 @@
+﻿namespace Magazine.Domain.AggregatesModel;
+public class Magazine
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+    public DateTime PublishDate { get; set; }
+}
---
+++ src/Magazine/Magazine.Domain/AggregatesModel/PublishingHouse/PublishingHouse.cs
@@ -0,0 +1,7 @@
+﻿namespace Magazine.Domain.AggregatesModel;
+public class PublishingHouse
+{
+    public Guid Id { get; init; }
+    public string Name { get; set; } = null!;
+    public DateTime FoundationYear { get; set; }
+}
