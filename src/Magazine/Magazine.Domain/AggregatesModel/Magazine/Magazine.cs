35914604fc59a6b1f2900ac0b4b4a05cc33d95d7
Axerm <aleksqq96@gmail.com>
2024-05-03 18:34:21 +0700

    update: обновлены доменные модели микросервиса Magazine

--- src/Magazine/Magazine.Domain/AggregatesModel/Magazine/Magazine.cs
+++ src/Magazine/Magazine.Domain/AggregatesModel/Magazine/Magazine.cs
@@ -3,14 +3,17 @@
 public class Magazine
 {
     public Guid Id { get; init; }
-    
+
     public string Name { get; init; } = null!;
-    
+
     public string? Description { get; set; }
-    
+
     public DateTime PublishDate { get; init; }
-    
+
     public PublishingHouse PublishingHouse { get; init; } = null!;
+
+    public override bool Equals(object? obj)
+        => Equals(obj as Magazine);
 
     public bool Equals(Magazine? magazine)
         => magazine != null &&
@@ -18,6 +21,13 @@ public class Magazine
             magazine.Name == Name &&
             magazine.PublishDate == PublishDate;
 
-    public override bool Equals(object? obj) 
-        => base.Equals(obj) ? true : Equals(obj as Magazine);
+    public override int GetHashCode()
+        => HashCode.Combine(Id, Name, Description, PublishDate, PublishingHouse);
+
+    public static bool operator ==(Magazine? a, Magazine? b)
+        => (a is null && b is null) ||
+           (a is not null && a.Equals(b));
+
+    public static bool operator !=(Magazine? a, Magazine? b)
+        => !(a == b);
 }
--- src/Magazine/Magazine.Domain/AggregatesModel/PublishingHouse/PublishingHouse.cs
+++ src/Magazine/Magazine.Domain/AggregatesModel/PublishingHouse/PublishingHouse.cs
@@ -2,31 +2,74 @@
 
 public class PublishingHouse
 {
-    private readonly IList<Magazine> _magazines = [];
-    
-    public Guid Id { get; init; }
-    
-    public string Name { get; init; } = null!;
-    
-    public DateTime FoundationYear { get; init; }
-    
-    public IReadOnlyCollection<Magazine> Magazines => _magazines.AsReadOnly();
+    protected readonly IList<Magazine> MagazinesRegistry = [];
 
-    public void AddMagazine(Magazine magazine)
+    public Guid Id { get; init; }
+
+    public string Name { get; init; } = null!;
+
+    public DateTime FoundationYear { get; init; }
+
+    /// <summary>
+    /// Список журналов, которые публиковались в данном издании.
+    /// </summary>
+    public IReadOnlyCollection<Magazine> Magazines => MagazinesRegistry.AsReadOnly();
+
+    protected virtual bool HasMagazine(Magazine magazine)
     {
-        if (magazine == null) return;
-        if (HasMagazine(magazine)) return;
-        _magazines.Add(magazine);
-    }
-    
-    private bool HasMagazine(Magazine magazine)
-    {
-        if (_magazines.Count == 0) return false;
-        foreach (Magazine innerMagazine in _magazines)
+        if (MagazinesRegistry.Count == 0 || magazine == null) return false;
+
+        foreach (Magazine innerMagazine in MagazinesRegistry)
         {
             if (innerMagazine.Equals(magazine)) return true;
         }
 
         return false;
     }
+
+    public virtual void AddMagazine(Magazine magazine)
+    {
+        if (magazine == null || HasMagazine(magazine)) return;
+        MagazinesRegistry.Add(magazine);
+    }
+
+    public virtual void RemoveMagazine(Magazine magazine)
+    {
+        if (magazine == null || !HasMagazine(magazine)) return;
+        MagazinesRegistry.Remove(magazine);
+    }
+
+    public override bool Equals(object? obj)
+        => Equals(obj as PublishingHouse);
+
+    public bool Equals(PublishingHouse? other)
+    {
+        if (other == null/* || MagazinesRegistry.Count != other.Magazines.Count*/)
+            return false;
+
+        //int intersectCount = Enumerable
+        //    .Intersect(Magazines, other.Magazines)
+        //    .Count();
+
+        // Если есть хотя бы одно совпадение в изданных журналах, издательства считаются равными.
+        bool hasMatch = Magazines.Any(
+            inner => other.Magazines.Any(
+                outer => inner.Id == outer.Id));
+
+        return /*intersectCount == MagazinesRegistry.Count*/
+            hasMatch &&
+            Id == other.Id &&
+            Name == other.Name &&
+            FoundationYear == other.FoundationYear;
+    }
+
+    public override int GetHashCode()
+        => HashCode.Combine(MagazinesRegistry, Id, Name, FoundationYear);
+
+    public static bool operator ==(PublishingHouse? a, PublishingHouse? b)
+        => (a is null && b is null) ||
+           (a is not null && a.Equals(b));
+
+    public static bool operator !=(PublishingHouse? a, PublishingHouse? b)
+        => !(a == b);
 }
