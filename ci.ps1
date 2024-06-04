4e4beeddcbd0168733e73c724437c1ce5f801083
Моор Михаил Александрович <m.moor@geomix.ru>
2024-06-04 09:03:13 +0700

    feature: настроен ci через скрипт powershell

---
+++ ci.ps1
@@ -0,0 +1,9 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Переменные для отправки в телегу
+$botToken = "7463441241:AAGN2_tjL8aJctKQ8p1CfjKW61GkA48u7Kg"
+$chatId = "-1002125306971"
+$messageText = "Hello World from PowerShell"
+$url="https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatID&text=$messageText"
+Invoke-RestMethod -Uri $url -Method Post
+
